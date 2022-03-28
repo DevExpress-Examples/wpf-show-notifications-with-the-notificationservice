ed374ebfdb78df40a7f55e269a0d466211f0aa25
dxgoncharov <darthvasilius@gmail.com>
2022-03-28 17:55:09 +0400

    clean-vb

--- VB/DXSampleNotificationSevice/ViewModel/CustomNotificationViewModel.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports DevExpress.Mvvm.DataAnnotations
-
-Namespace DXSampleNotificationSevice.ViewModel
-	<POCOViewModel>
-	Public Class CustomNotificationViewModel
-		Public Overridable Property Caption() As String
-		Public Overridable Property Content() As String
-	End Class
-End Namespace
