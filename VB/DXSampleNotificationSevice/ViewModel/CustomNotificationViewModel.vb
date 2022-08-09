8a8392e57a301811342398fc684f56268c30d069
DevExpressExampleBot <fakeEmail>
2022-08-09 18:20:46 +0400

    Source auto update [skip ci]

--- VB/DXSampleNotificationSevice/ViewModel/CustomNotificationViewModel.vb
+++ VB/DXSampleNotificationSevice/ViewModel/CustomNotificationViewModel.vb
@@ -1,9 +1,12 @@
-﻿Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace DXSampleNotificationSevice.ViewModel
-	<POCOViewModel>
-	Public Class CustomNotificationViewModel
-		Public Overridable Property Caption() As String
-		Public Overridable Property Content() As String
-	End Class
+
+    <POCOViewModel>
+    Public Class CustomNotificationViewModel
+
+        Public Overridable Property Caption As String
+
+        Public Overridable Property Content As String
+    End Class
 End Namespace
