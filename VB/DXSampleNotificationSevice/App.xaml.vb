cd3c1023f9afececfd0797f162b0bffebf4e4981
DevExpressExampleBot <fakeEmail>
2022-08-09 18:23:59 +0400

    Source auto update [skip ci]

---
+++ VB/DXSampleNotificationSevice/App.xaml.vb
@@ -0,0 +1,13 @@
+Imports System.Windows
+
+Namespace DXSampleNotificationSevice
+
+    Public Partial Class App
+        Inherits Application
+
+        Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
+            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
+            DevExpress.Data.ShellHelper.TryCreateShortcut("sample_notification_app", "DXSampleNotificationSevice")
+        End Sub
+    End Class
+End Namespace
--- VB/DXSampleNotificationSevice/ViewModel/MainViewModel.vb
+++ VB/DXSampleNotificationSevice/ViewModel/MainViewModel.vb
@@ -1,35 +1,37 @@
-﻿Imports System
 Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.POCO
 
 Namespace DXSampleNotificationSevice.ViewModel
-	<POCOViewModel>
-	Public Class MainViewModel
-		<ServiceProperty(Key := "ServiceWithDefaultNotifications")>
-		Protected Overridable ReadOnly Property DefaultNotificationService() As INotificationService
-			Get
-				Return Nothing
-			End Get
-		End Property
-		<ServiceProperty(Key := "ServiceWithCustomNotifications")>
-		Protected Overridable ReadOnly Property CustomNotificationService() As INotificationService
-			Get
-				Return Nothing
-			End Get
-		End Property
 
-		Public Sub ShowDefaultNotification()
-			Dim notification As INotification = DefaultNotificationService.CreatePredefinedNotification("Predefined Notification", "First line", String.Format("Second line. Time: {0}", DateTime.Now), Nothing)
-			notification.ShowAsync()
-		End Sub
+    <POCOViewModel>
+    Public Class MainViewModel
 
-		Public Sub ShowCustomNotification()
-			Dim vm As CustomNotificationViewModel = ViewModelSource.Create(Function() New CustomNotificationViewModel())
-			vm.Caption = "Custom Notification"
-			vm.Content = String.Format("Time: {0}", DateTime.Now)
-			Dim notification As INotification = CustomNotificationService.CreateCustomNotification(vm)
-			notification.ShowAsync()
-		End Sub
-	End Class
+        <ServiceProperty(Key:="ServiceWithDefaultNotifications")>
+        Protected Overridable ReadOnly Property DefaultNotificationService As INotificationService
+            Get
+                Return Nothing
+            End Get
+        End Property
+
+        <ServiceProperty(Key:="ServiceWithCustomNotifications")>
+        Protected Overridable ReadOnly Property CustomNotificationService As INotificationService
+            Get
+                Return Nothing
+            End Get
+        End Property
+
+        Public Sub ShowDefaultNotification()
+            Dim notification As INotification = DefaultNotificationService.CreatePredefinedNotification("Predefined Notification", "First line", String.Format("Second line. Time: {0}", Date.Now), Nothing)
+            notification.ShowAsync()
+        End Sub
+
+        Public Sub ShowCustomNotification()
+            Dim vm As CustomNotificationViewModel = ViewModelSource.Create(Function() New CustomNotificationViewModel())
+            vm.Caption = "Custom Notification"
+            vm.Content = String.Format("Time: {0}", Date.Now)
+            Dim notification As INotification = CustomNotificationService.CreateCustomNotification(vm)
+            notification.ShowAsync()
+        End Sub
+    End Class
 End Namespace
